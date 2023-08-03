b3e1dde3b57e90598c6c3dc170dba175b017cbac
naeem <naeemislamjr182@gmail.com>
2023-08-03 22:50:38 +0600

    simple return

---
+++ retern.js
@@ -0,0 +1,10 @@
+function singarashop(money) {
+
+    var sin_price = 10;
+    var quantity = money / sin_price;
+    return quantity;
+}
+
+
+var tk = singarashop(535);
+console.log("kayta khaisi", tk);
