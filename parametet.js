166463770ae20543f28ff6f33afcc29c3275c8ed
naeem <naeemislamjr182@gmail.com>
2023-08-03 20:22:44 +0600

    function and parameter

---
+++ parametet.js
@@ -0,0 +1,11 @@
+//fundtion call
+function misti_niya_ay(tk) {
+    console.log("wait half an hour");
+    console.log("mama 2kg misty den");
+    console.log(tk, "tk de age");
+
+
+
+}
+// call the function
+misti_niya_ay(500)
