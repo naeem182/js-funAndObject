cf09951413abd1bbd38fcd4f85640d32bd1914ba
naeem <naeemislamjr182@gmail.com>
2023-08-04 01:45:01 +0600

    object

---
+++ object.js
@@ -0,0 +1,45 @@
+
+const student = {
+    id: 115,
+    name: 'Solamin Khan',
+    class: 9,
+    marks: 98
+}
+
+const mobile = {
+    brand: 'Samsung',
+    price: 19000,
+    storage: '64gb',
+    camera: '7MP'
+}
+
+
+var myComputer = {
+    brand: 'lonovo',
+    price: 39000,
+    color: 'silver',
+    processor: 'i7'
+}
+
+
+// // console.log(myComputer)
+// // console.log(myComputer.price)//obj.property
+// // //keys
+// const keys = Object.keys(mobile);
+// // console.log(keys);
+
+
+// for (let i = 0; i < keys.length; i++) {
+//     const key = keys[i]
+//     console.log(mobile)
+// }
+
+
+// for (let key of keys) {
+//     console.log(mobile[key])
+// }
+// // //values
+// // const value = Object.values(mobile);
+
+// console.log(value);
+
