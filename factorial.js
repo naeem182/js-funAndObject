cc8e19eefb625f283f22499df301d112b082f1c1
naeem <naeemislamjr182@gmail.com>
2023-08-05 03:28:40 +0600

    odd add and even add with function

--- factorial.js
+++ factorial.js
@@ -7,3 +7,5 @@ function factorial(num) {
         return num * factorial(num - 1);
 }
 console.log(factorial(0))
+
+
--- onlyevenadd.js
+++ onlyevenadd.js
@@ -10,6 +10,20 @@ function evenAdd(arr) {
     return sum;
 
 }
-let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0,];
+function oddAdd(arr) {
+    let osum = 0;
+    for (let iteams of arr) {
+        if (iteams % 2 == 1) {
+            osum += iteams;
+            console.log(iteams);
+        }
+
+    }
+    return osum;
+
+}
+let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 12,];
 let total = evenAdd(num);
+let oddtotal = oddAdd(num);
 console.log("only even num total", total);
+console.log("only even odd total", oddtotal);
